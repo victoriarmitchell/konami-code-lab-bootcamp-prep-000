499e2ba70a82d6e5fa300c315136cf1323468951
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-12 21:12:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,30 +11,22 @@ const codes = [
   "a"
 ];
 
-
 function init() {
-  // your code here
-  const body = document.querySelector('body');
-  body.addEventListener('keydown', onKeyDownHandler);
-}
- 
-// Keep track of index outside of the event handler.
-let index = 0;
- 
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = e.key;
- 
-  if (key === codes[index]) {
-    index++;
- 
-    if (index === codes.length) {
-      alert("Enjoy your Easter egg!");
- 
+  let index = 0;
+  document.body.addEventListener('keydown', codeHandler);
+  
+  function codeHandler(e) {
+    const key = e.key;
+    if(key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        alert('Congratulations! You have entered the konami code and won 3 wishes!')
+        
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
   }
 }
-
